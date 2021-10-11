33a86c4e1c53034bfd780c6a95e857bc837cb1d0
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-11 11:20:55 +0530

    ok

---
+++ Day20UserRegistrationUseCase1/UseCase1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day20UserRegistrationUseCase1
+{
+    public class UseCase1
+    {
+        public void insertData()
+        {
+            Regex reg = new Regex("^([A-Z]+[A-Za-z)");
+            Console.WriteLine("Enter first name");
+            string fname = Console.ReadLine();
+            if (reg.IsMatch(fname) && fname.Length >= 3)
+                Console.WriteLine("Your name is : " + fname);
+            else
+                Console.WriteLine("Enter Valid Name");
+
+        }
+    }
+}
