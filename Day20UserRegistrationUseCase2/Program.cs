6b8d299cfadc8885abb0f20ba78a9dd3edcdb49e
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-11 11:24:43 +0530

    ok

---
+++ Day20UserRegistrationUseCase2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day20UserRegistrationUseCase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase2 obj = new UseCase2();
+            obj.insertData();
+        }
+    }
+}
