0f467d0a3c3a7bde8116da9e24bae8e3e384c7fa
vaibhav347 <36009358+vaibhav347@users.noreply.github.com>
2021-10-11 11:44:45 +0530

    Update UseCase1.cs

--- Day20UserRegistrationUseCase1/UseCase1.cs
+++ Day20UserRegistrationUseCase1/UseCase1.cs
@@ -9,7 +9,7 @@ namespace Day20UserRegistrationUseCase1
     {
         public void insertData()
         {
-            Regex reg = new Regex("^([A-Z]+[A-Za-z)");
+            Regex reg = new Regex("^([A-Z]+[A-Za-z])");
             Console.WriteLine("Enter first name");
             string fname = Console.ReadLine();
             if (reg.IsMatch(fname) && fname.Length >= 3)
